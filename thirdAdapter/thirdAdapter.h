214bd5cf9a9a45e62e1f04601f9ad3d37787c3f3
shuai.liu.dev <shuai.liu.dev@ihandysoft.com>
2018-05-29 17:26:44 +0800

    Initial Commit

---
+++ thirdAdapter/thirdAdapter.h
@@ -0,0 +1,19 @@
+//
+//  thirdAdapter.h
+//  thirdAdapter
+//
+//  Created by shuai.liu on 2018/5/29.
+//  Copyright © 2018年 shuai.liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for thirdAdapter.
+FOUNDATION_EXPORT double thirdAdapterVersionNumber;
+
+//! Project version string for thirdAdapter.
+FOUNDATION_EXPORT const unsigned char thirdAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <thirdAdapter/PublicHeader.h>
+
+
