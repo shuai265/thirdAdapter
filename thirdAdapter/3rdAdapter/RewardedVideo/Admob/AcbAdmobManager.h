45bd7671dc14e0e242ea1504ad0acf56d391b0e6
shuai.liu.dev <shuai.liu.dev@ihandysoft.com>
2018-05-29 19:09:21 +0800

    first commit thirdadapter

---
+++ thirdAdapter/3rdAdapter/RewardedVideo/Admob/AcbAdmobManager.h
@@ -0,0 +1,13 @@
+//
+//  AcbAdmobManager.h
+//  thirdAdapter
+//
+//  Created by shuai.liu on 2018/5/29.
+//  Copyright © 2018年 shuai.liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AcbAdmobManager : NSObject
+
+@end
---
+++ thirdAdapter/3rdAdapter/RewardedVideo/Admob/AcbAdmobManager.m
@@ -0,0 +1,13 @@
+//
+//  AcbAdmobManager.m
+//  thirdAdapter
+//
+//  Created by shuai.liu on 2018/5/29.
+//  Copyright © 2018年 shuai.liu. All rights reserved.
+//
+
+#import "AcbAdmobManager.h"
+
+@implementation AcbAdmobManager
+
+@end
